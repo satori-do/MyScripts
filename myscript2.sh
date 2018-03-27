6f424bacfe4e7b3990d2877ffbe3e39e9182748a
satori-do <37803153+satori-do@users.noreply.github.com>
2018-03-27 11:26:20 +0300

    Add files via upload

---
+++ myscript2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mycomputer="Scorpio"
+myOS=`uname -a`
+
+echo "This script name is $0"
+echo "Privet $1"
+echo "Hello $2"
+
+num1=19
+num2=79
+summa=$((num1+num2))
+
+echo "$num1 + $num2 = $summa"
