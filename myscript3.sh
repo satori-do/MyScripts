8734e01e313e95cb890f3600d26e13184fbb25d0
satoi-do <volodymyrreznich@gmail.com>
2018-03-28 11:46:22 +0300

    Add echo 0

--- myscript3.sh
+++ myscript3.sh
@@ -11,9 +11,12 @@ echo "Starting CASE selection..."
 case $x in
 	1) echo "This is one";;
      [2-9]) echo "Two-Nine";;
-    "Петя") echo "Привет $x";;
+   "Петя") echo "Привет $x";;
 	*) echo "Parment Unknov, sorry!"
 esac
+
+echo $?
+
 if [ $? -eq 0 ]; then echo "Все ok"
  # do something
 else echo "Щось не так..."
