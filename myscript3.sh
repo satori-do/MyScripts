34bd214c05588152ad555d90b5dfa0060b507ffc
satoi-do <volodymyrreznich@gmail.com>
2018-03-28 10:31:45 +0300

    Comlete script

--- myscript3.sh
+++ myscript3.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
 if ["$1" == "Vova"]; then echo "Привіт $1"
-elif ["$1" == "Bob"]; then echo "Hello $1"
+elif [ "$1" == "Bob" ]; then echo "Hello $1"
+else echo "Who are you?"
 fi
+
+x=$2
+
+echo "Starting CASE selection..."
+case $x in
+<------> 1) echo "This is one";;
+     [2-9]) echo "Two-Nine";;
+    "Петя") echo "Привет";;
+<------> *) echo "Parment Unknov, sorry!"
+esac
+
