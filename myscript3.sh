0284f73e88d839569233c13ab72025765ca4478e
satori-do <volodymyrreznich@gmail.com>
2019-03-25 14:30:30 +0200

    Reading parameters.

--- myscript3.sh
+++ myscript3.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-if [ "$1" == "Вова" ]; then echo "Привіт $1"
-elif [ "$1" == "Bob" ]; then echo "Hello $1"
-else echo "Who are you?"
-fi
+#-------------------------------------------------------------------------------------------------------------------
+echo "# reading parameters:"
 
-x=$2
+echo $0 # Script name
 
-echo "Starting CASE selection..."
-case $x in
-	1) echo "This is one";;
-     [2-9]) echo "Two-Nine";;
-   "Петя") echo "Привет $x";;
-	*) echo "Parment Unknov, sorry!"
-esac
+echo $1 # 1st parameter
 
-echo $?
+echo $2 # 2nd parameter
 
-if [ $? -eq 0 ]; then echo "Все ok"
- # do something
-else echo "Щось не так..."
- # do something else
-fi
+echo $3 # 3rd parameter
+
+#-------------------------------------------------------------------------------------------------------------------
