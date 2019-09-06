39cf15c93e6ea56cf9575334da90d4b1797662e1
satori-do <volodymyrreznich@gmail.com>
2019-09-06 13:58:07 +0300

    Based command.

--- myscript1.sh
+++ myscript1.sh
@@ -2,4 +2,14 @@
 
 set -e
 
-echo "Hello World!!!"
+echo "*"
+echo "[Who is logged in:]"
+who # who is logged in
+
+echo "[The username and his current terminal:]"
+mesg
+
+echo "[Checking any logged user message status:]"
+who -T
+
+echo "*"
