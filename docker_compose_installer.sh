e64dd1732f673c4f9ad4817967daac63b43efc9b
satori-do <volodymirreznich@gmail.com>
2019-11-10 16:27:18 +0200

    Was added installer for docker and compose.

---
+++ docker_compose_installer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
+
+docker --version
+
+curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
